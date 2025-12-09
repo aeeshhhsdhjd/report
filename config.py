fc0e1cfd6e09c712cb9686a156bb9a74bbf7ab4f
Yash Samrat <yashsamrat10080@gmail.com>
2025-12-09 19:20:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
-import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-API_ID = int(os.getenv("API_ID", "0"))
-API_HASH = os.getenv("API_HASH", "")
-MONGO_URI = os.getenv("MONGO_URI", "")
+# ============================
+#   DIRECT CONFIG VALUES
+# ============================
+
+BOT_TOKEN = "8560252019:AAGEn7LTiKXdiVHMc7eHuXDm0rAuLdvZNUo"
+API_ID = 27989579  # yahan apna API ID (number) daal
+API_HASH = "64742ebe270a7d202150134d66397839"
+MONGO_URI = "mongodb+srv://annieregain:firstowner8v@anniere.ht2en.mongodb.net/?retryWrites=true&w=majority&appName=AnnieRE"
